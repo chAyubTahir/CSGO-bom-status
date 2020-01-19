281057de8c51ccc419a97024ab3d2db570e35009
chAyubTahir <55590150+chAyubTahir@users.noreply.github.com>
2020-01-19 11:09:45 +0100

    Add files via upload

---
+++ bomb.ino
@@ -0,0 +1,46 @@
+//this code works with th API from counter strike game. When the bomb is
+planted in the game RGB LED lights change there colour from GREEN to RED
+
+int data;
+int x;
+void setup() {
+  Serial.begin(115200);
+  pinMode(11, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(9, OUTPUT);
+  Serial.println ("Hi");
+}
+
+void loop() {
+x=1;
+analogWrite (10,100);
+while (x==1) {
+  data= Serial.read();
+  if (data== '6'){
+    analogWrite (10,180);
+    analogWrite (9,0);
+    analogWrite (11,0);
+    }
+if (data== '4'){
+  
+ analogWrite (11,255);
+ analogWrite (10,0);
+ analogWrite (9,0);
+  }
+if(data=='3'){
+   analogWrite (9,255);
+analogWrite (11,0);
+analogWrite (10,0);
+}
+if (data== '5'){
+analogWrite (9,0);
+analogWrite (10,0);
+analogWrite (11,0);
+analogWrite (9,255);
+analogWrite (10,255);
+analogWrite (11,255);
+
+}
+}
+}
+
